df61316539f01afda1b641ed25b7f70d2b1bcdd6
Amir <amirmehboob921@gmail.com>
2024-11-29 10:52:55 -0800

    customize theme

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,9 @@
-import useData from "./useData";
+import genres from "../data/genres";
 export interface Genres{
     id: number;
     name: string
     image_background:string
 }
 
-const useGenres = () => useData<Genres>('/genres')
+const useGenres = () => ({data:genres,isLoading:false,errors:null})
 export default useGenres;
